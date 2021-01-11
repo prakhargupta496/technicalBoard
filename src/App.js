a46f653672eca23f0db45721ca37c7c964cd1f08
Ankit Kumar <53820439+iamankitkumar08062001@users.noreply.github.com>
2021-01-11 17:52:46 +0530

    Changed Project Structure to correspond to multi-page Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import Header from './Pages/Main/Header';
-import Navigation from './Common/Navigation';
-import About from './Pages/Main/About';
+import Avant from './pages/Avant';
+import Events from './pages/Event';
 
 class App extends React.Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                {/* This component will render the heading in the form:-
-                Name
-                (Alternative Name)
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-                If none of the above are provided, then the component will
-                only generate a link with an Image */}
-                <Navigation />
-                <Header name = "Avant" altName = "Programming Club" link = "#"/>
-                <About />
-            </div>
+    render() { 
+        return (
+            <Avant />
         );
     }
 }
---
+++ src/pages/Event.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Avant() {
+    return ( 
+        <div>
+            <h1>Events</h1>
+        </div>
+    );
+}
+
+export default Avant;
