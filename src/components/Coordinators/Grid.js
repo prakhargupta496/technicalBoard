688e48761cee78bc53e370176391b4b0084e7f59
Ankit Kumar <53820439+iamankitkumar08062001@users.noreply.github.com>
2021-01-19 14:00:40 +0530

    Added Coordinators

--- src/components/Coordinators/Grid.js
+++ src/components/Coordinators/Grid.js
@@ -10,27 +10,33 @@ class Grid extends React.Component {
         this.state = [ 
             {
                 image: "https://images.unsplash.com/photo-1610288291998-366d446646cc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1188&q=80",
-                description: "Event1"
+                name: "Name1",
+                caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut elementum odio, in tristique ante. Ut accumsan fringilla velit quis porta. Sed tellus nibh, dictum vel consectetur sit amet, tempus maximus ex. Pellentesque sit amet libero tristique, consequat mauris eu, posuere neque. Curabitur dapibus dolor nisi, sed tempus diam euismod et.",
             },
             {
                 image: "https://images.unsplash.com/photo-1566875276423-ab730125662f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
-                description: "Event2"
+                name: "Name2",
+                caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut elementum odio, in tristique ante. Ut accumsan fringilla velit quis porta. Sed tellus nibh, dictum vel consectetur sit amet, tempus maximus ex. Pellentesque sit amet libero tristique, consequat mauris eu, posuere neque. Curabitur dapibus dolor nisi, sed tempus diam euismod et.",
             },
             {
                 image: "https://images.unsplash.com/photo-1609516002784-4eca99ebe39f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1190&q=80",
-                description: "Event3"
+                name: "Name3",
+                caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut elementum odio, in tristique ante. Ut accumsan fringilla velit quis porta. Sed tellus nibh, dictum vel consectetur sit amet, tempus maximus ex. Pellentesque sit amet libero tristique, consequat mauris eu, posuere neque. Curabitur dapibus dolor nisi, sed tempus diam euismod et.",
             },
             {
                 image: "https://images.unsplash.com/photo-1606040185180-1f38c31e74a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-                description: "Event4"
+                name: "Name4",
+                caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut elementum odio, in tristique ante. Ut accumsan fringilla velit quis porta. Sed tellus nibh, dictum vel consectetur sit amet, tempus maximus ex. Pellentesque sit amet libero tristique, consequat mauris eu, posuere neque. Curabitur dapibus dolor nisi, sed tempus diam euismod et.",
             },
             {
                 image: "https://images.unsplash.com/photo-1610288291998-366d446646cc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1188&q=80",
-                description: "Event5"
+                name: "Name5",
+                caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut elementum odio, in tristique ante. Ut accumsan fringilla velit quis porta. Sed tellus nibh, dictum vel consectetur sit amet, tempus maximus ex. Pellentesque sit amet libero tristique, consequat mauris eu, posuere neque. Curabitur dapibus dolor nisi, sed tempus diam euismod et.",
             },
             {
                 image: "https://images.unsplash.com/photo-1566875276423-ab730125662f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
-                description: "Event6"
+                name: "Name6",
+                caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut elementum odio, in tristique ante. Ut accumsan fringilla velit quis porta. Sed tellus nibh, dictum vel consectetur sit amet, tempus maximus ex. Pellentesque sit amet libero tristique, consequat mauris eu, posuere neque. Curabitur dapibus dolor nisi, sed tempus diam euismod et.",
             }
         ];
     }
@@ -42,7 +48,7 @@ class Grid extends React.Component {
                     {
                         this.state.map(item => {
                             return (
-                                <Item key={item.description} src={item.image} />
+                                <Item key={item.title} data={item} />
                             );
                         })
                     }
--- src/components/Coordinators/Item.js
+++ src/components/Coordinators/Item.js
@@ -1,37 +1,24 @@
 import React from 'react';
-import { Card, Col, ListGroup } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 import Styles from './item.module.css';
-import { FaFacebookSquare as Facebook, FaLinkedin as LinkedIn, FaInstagram as Instagram, FaGithub as GitHub, FaGlobe as Globe } from "react-icons/fa";
+import Icons from './Icons';
 
 function Entry(props) {
     return (
         <Col md={4} className={Styles.entry}>
             <Card className={Styles.card}>
-                <Card.Img src={props.src} alt="Card image" className={Styles.image} />
+                <Card.Img src={props.data.image} alt="Card image" className={Styles.image} />
                 <Card.ImgOverlay as={motion.div} className={Styles.overlay} whileHover={{
                     opacity: 1,
                     transition: { duration: 0.2 },
                 }}>
                     <Card.Body className={Styles.content}>
-                        <Card.Title as="h1" className={Styles.heading}>Card title</Card.Title>
-                        <ListGroup horizontal>
-                            <ListGroup.Item>
-                                <Facebook className={Styles.icon}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Instagram className={Styles.icon}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <LinkedIn className={Styles.icon}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <GitHub className={Styles.icon}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Globe className={Styles.icon}/>
-                            </ListGroup.Item>
-                        </ListGroup>
+                        <Card.Title as="h1" className={Styles.heading}>{props.data.name}</Card.Title>
+                        <Card.Text>
+                            {props.data.caption}
+                        </Card.Text>
+                        <Icons />
                     </Card.Body>
                 </Card.ImgOverlay>
             </Card>
