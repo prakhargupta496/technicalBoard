d321c0fae386841a6e45e0827b5bd43866dd9d87
ayushchou8876 <choudhuryaayush@gmail.com>
2021-01-24 21:15:26 +0530

    Added env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-    apiKey: "AIzaSyCkvCRZ2w5yNfYqtiCPEyiSV0_FrVDmOHQ",
+    apiKey: process.env.API_KEY,
     authDomain: "technical-board.firebaseapp.com",
     projectId: "technical-board",
     storageBucket: "technical-board.appspot.com",
-    messagingSenderId: "240946019514",
-    appId: "1:240946019514:web:d438e16f1539bd5be5d683",
-    measurementId: "G-W5TGT5V8G3"
+    messagingSenderId: process.env.MESSAGE_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASURE_ID
 };
 
 // Initialize Firebase
