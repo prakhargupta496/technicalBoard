5de5329e51bc29195a8bacc3a0e3173ad997e7e6
ayushchou8876 <choudhuryaayush@gmail.com>
2021-01-25 17:08:43 +0530

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,18 +7,30 @@ import Styles from './contact.module.css';
 
 class Contact extends React.Component {
     
-    state = {  }
+    constructor(props){
+        super(props);
+        //this.handleChange = this.handleChange.bind(this);
+    }
+    state = { 
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    }
+
     render() { 
-        return (<Section id="contactUs" style={this.props.style}>
+        return (
+        <Section id="contactUs" style={this.props.style}>
         <Container fluid>
             <Row>
                 <Col className={Styles.form} md={6}>
                     <SectionHeader>Contact Us</SectionHeader>
                     <Form>
-                        <Form.Control className={`${Styles.input} ${Styles.small}`} type="text" placeholder="Name" />
-                        <Form.Control className={`${Styles.input} ${Styles.small}`} type="email" placeholder="Your E-Mail" />
-                        <Form.Control className={`${Styles.input} ${Styles.small}`} type="text" placeholder="Subject" />
-                        <Form.Control className={`${Styles.input} ${Styles.big}`} as="textarea" rows={5} placeholder="Message" />
+                        <Form.Control className={`${Styles.input} ${Styles.small}`} type="text" placeholder="Name" value={this.state.name} 
+                        onChange={(e) => this.setState({name: e.target.value})} />
+                        <Form.Control className={`${Styles.input} ${Styles.small}`} type="email" placeholder="Your E-Mail" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />
+                        <Form.Control className={`${Styles.input} ${Styles.small}`} type="text" placeholder="Subject" value={this.state.subject} onChange={(e) => this.setState({subject: e.target.value})} />
+                        <Form.Control className={`${Styles.input} ${Styles.big}`} as="textarea" rows={5} placeholder="Message" value={this.state.message} onChange={(e) => this.setState({message: e.target.value})} />
                         <Button className={Styles.button}>Submit</Button>
                     </Form>
                 </Col>
